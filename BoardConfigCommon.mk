e93c6ec3013947af9c90515548aaf695cc22b284
saxmydix <linexsg@gmail.com>
2018-07-02 06:37:18 +0000

    havocify
    Change-Id: Iae8e653159bb85c36cacc872a509d31264d11b25

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -3,6 +3,12 @@
 
 COMMON_PATH := device/samsung/star-common
 
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Platform
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := exynos5
